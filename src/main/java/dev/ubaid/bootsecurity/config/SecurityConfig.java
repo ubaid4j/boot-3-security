276fdbcfa6e0ce39e5692aeebf5a931dfce74c8f
ubaid4j <ubaidshan007@gmail.com>
2023-02-03 10:26:42 +0500

    - improved logging (add file line number) - still r&d on open reqeusts

--- src/main/java/dev/ubaid/bootsecurity/config/SecurityConfig.java
+++ src/main/java/dev/ubaid/bootsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -29,12 +30,17 @@ public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.securityMatcher("/**")
-            .csrf().disable()
+        http
             .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/**").permitAll()
                 .anyRequest()
-                .permitAll()
-            );
+                .authenticated()
+            ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/api/open/**", "/api/open/**");
     }
 }
